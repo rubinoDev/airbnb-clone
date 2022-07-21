0e7cb1c517c02808f66993e147a496eb9aeef16e
Victor Rubiño <105171818+rubinoDev@users.noreply.github.com>
2022-07-21 16:28:31 -0300

    require

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export function Hero(){
   return(
       <>
-      <img className="hero__img" src="../src/assets/illustration.png"/>
+      <img className="hero__img" src={require ("/src/assets/illustration.png")}/>
         <h1 className="hero__h1">Online Experiences</h1>
       <div className="hero__p-container">
         <p className="hero__p">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
